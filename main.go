bc1ce2cb1a787fe4c276169bf063ca7054c84df4
Vitaly <vitalynesterov28@gmail.com>
2026-03-01 18:36:27 +0300

    Раздел про функции

--- main.go
+++ main.go
@@ -6,9 +6,22 @@ import (
 )
 
 func main() {
+	userWeight, userHeight := GetUserInput()
+	fmt.Print(Result(userWeight, userHeight))
+}
+
+func Result(userWeight float64, userHeight float64) string {
 	const IMTPower = 2
-	userHeight := 1.8
-	var userWeight float64 = 95
 	var IMT = userWeight / math.Pow(userHeight, IMTPower)
-	fmt.Print(IMT)
+	return fmt.Sprintf("Ваш индекс массы тела: %.1f", IMT)
+}
+
+func GetUserInput() (float64, float64) {
+	var userHeight float64
+	var userWeight float64
+	fmt.Print("Введите свой рост(м): ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес(кг): ")
+	fmt.Scan(&userWeight)
+	return userWeight, userHeight
 }
