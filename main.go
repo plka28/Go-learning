a416d87665daac32be640e2c3c8ad09346408bce
Vitaly <vitalynesterov28@gmail.com>
2026-03-01 11:15:11 +0300

    Первый урок

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Print("Hello World!")
+	const IMTPower = 2
+	userHeight := 1.8
+	var userWeight float64 = 95
+	var IMT = userWeight / math.Pow(userHeight, IMTPower)
+	fmt.Print(IMT)
 }
