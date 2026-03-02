c2d6b2c43c3805ae763dc631773ee53c1601454c
Vitaly <vitalynesterov28@gmail.com>
2026-03-02 17:29:19 +0300

    Закончил пятый раздел

--- main.go
+++ main.go
@@ -1,19 +1,57 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 )
 
 func main() {
-	userWeight, userHeight := GetUserInput()
-	fmt.Print(Result(userWeight, userHeight))
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("recover", r)
+		}
+	}()
+	i := "Yes"
+	for i != "No" {
+		userWeight, userHeight := GetUserInput()
+		IMT, err := calculateResult(userWeight, userHeight)
+		if err != nil {
+			// fmt.Println("Ошибка в вводе данных")
+			// continue
+			panic("Ошибка в вводе данных")
+		}
+		outputResult(IMT)
+		fmt.Println("Продолжить рассчет индекса массы тела?(Yes/No)")
+		fmt.Scan(&i)
+		for i != "Yes" && i != "No" {
+			fmt.Println("Неверный ввод.")
+			fmt.Println("Продолжить рассчет индекса массы тела?(Yes/No)")
+			fmt.Scan(&i)
+		}
+	}
 }
-
-func Result(userWeight float64, userHeight float64) string {
+func calculateResult(userWeight, userHeight float64) (float64, error) {
 	const IMTPower = 2
-	var IMT = userWeight / math.Pow(userHeight, IMTPower)
-	return fmt.Sprintf("Ваш индекс массы тела: %.1f", IMT)
+	if userHeight <= 0 || userWeight <= 0 {
+		return 0, errors.New("INCORRECT_PARAMS")
+	}
+	return userWeight / math.Pow(userHeight, IMTPower), nil
+}
+func outputResult(IMT float64) {
+	fmt.Printf("Ваш индекс массы тела: %.1f\n", IMT)
+	switch {
+	case IMT < 16:
+		fmt.Println("Сильный недостаток веса")
+	case IMT < 18.5:
+		fmt.Println("Задохлик")
+	case IMT < 25:
+		fmt.Println("Норма")
+	case IMT < 30:
+		fmt.Println("На грани")
+	default:
+		fmt.Println("Ожирение")
+	}
 }
 
 func GetUserInput() (float64, float64) {
