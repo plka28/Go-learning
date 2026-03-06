c51052df26746ba7a9a0d4e7f218ac8d45e73987
Vitaly <vitalynesterov28@gmail.com>
2026-03-06 21:02:23 +0300

    Приложение для закладок

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module demo/app-2
+module demo/app-3
 
 go 1.25.0
--- main.go
+++ main.go
@@ -3,20 +3,53 @@ package main
 import "fmt"
 
 func main() {
-	transactions := []float64{}
-	var a float64
-	var sum float64
+	bookmark := map[string]string{}
 	for {
-		fmt.Print("Введиет свою транзакцию: ")
-		fmt.Scan(&a)
-		if a == 0 {
-			break
+		switch dialogue() {
+		case 1:
+			show(bookmark)
+		case 2:
+			bookmark = add(bookmark)
+		case 3:
+			bookmark = del(bookmark)
+		case 4:
+			return
 		}
-		transactions = append(transactions, a)
 	}
-	for _, value := range transactions {
-		sum += value
+}
+func dialogue() int {
+	for {
+		var choice int
+		fmt.Println("1. Посмотреть закладки")
+		fmt.Println("2. Добавить закладку")
+		fmt.Println("3. Удалить закладку")
+		fmt.Println("4. Выход")
+		fmt.Scan(&choice)
+		if choice == 1 || choice == 2 || choice == 3 || choice == 4 {
+			return choice
+		}
+		fmt.Println("Неправильный ввод")
 	}
-	fmt.Println(transactions)
-	fmt.Println(sum)
+}
+func show(bookmark map[string]string) {
+	for key, value := range bookmark {
+		fmt.Printf("Ключ: %s, Значение: %s\n", key, value)
+	}
+}
+func add(bookmark map[string]string) map[string]string {
+	var newKey string
+	var newValue string
+	fmt.Print("Введите ключ: ")
+	fmt.Scan(&newKey)
+	fmt.Print("Введите значение: ")
+	fmt.Scan(&newValue)
+	bookmark[newKey] = newValue
+	return bookmark
+}
+func del(bookmark map[string]string) map[string]string {
+	var newKey string
+	fmt.Print("Введите ключ: ")
+	fmt.Scan(&newKey)
+	delete(bookmark, newKey)
+	return bookmark
 }
