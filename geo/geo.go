758793583b717f4d8820f53389289082c2ecc584
Vitaly <vitalynesterov28@gmail.com>
2026-03-29 18:27:31 +0300

    занятие по http

---
+++ geo/geo.go
@@ -0,0 +1,65 @@
+package geo
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+type GeoData struct {
+	City string `json:"city"`
+}
+
+type CityPopulationResponce struct {
+	Error bool `json:"error"`
+}
+
+func GetMyLocation(city string) (*GeoData, error) {
+	if city != "" {
+		isCity := checkCity(city)
+		if !isCity {
+			panic("Такого города нет")
+		}
+		return &GeoData{
+			City: city,
+		}, nil
+	}
+	resp, err := http.Get("https://ipapi.co/json")
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		return nil, errors.New("NOT200")
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var geo GeoData
+	json.Unmarshal(body, &geo)
+	return &geo, nil
+}
+
+func checkCity(city string) bool {
+	postBody, _ := json.Marshal(map[string]string{
+		"city": city,
+	})
+	resp, err := http.Post("https://ipapi.co/json", "application/json", bytes.NewBuffer(postBody))
+	if err != nil {
+		return false
+	}
+	if resp.StatusCode != 200 {
+		return false
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return false
+	}
+	var populationResponce CityPopulationResponce
+	json.Unmarshal(body, &populationResponce)
+	return !populationResponce.Error
+}
--- main.go
+++ main.go
@@ -1,107 +1,23 @@
 package main
 
 import (
-	"demo/app-4/account"
-	"demo/app-4/encrypter"
-	"demo/app-4/files"
-	"demo/app-4/output"
+	"demo/app-5/geo"
+	"demo/app-5/weather"
+	"flag"
 	"fmt"
-	"strings"
-
-	"github.com/joho/godotenv"
 )
 
-var menu = map[string]func(*account.VaultWithDb){
-	"1": createAccount, "2": findAccountByUrl, "3": findAccountByLogin, "4": deleteAccount,
-}
-
 func main() {
-	dialogue()
-}
+	city := flag.String("city", "", "Город пользователя")
+	format := flag.Int("format", 1, "Формат вывода погоды")
 
-func createAccount(vault *account.VaultWithDb) {
-	login := promptData("Введите логин: ")
-	password := promptData("Введите пароль: ")
-	url := promptData("Введите url: ")
+	flag.Parse()
 
-	myAccount, err := account.NewAccount(login, password, url)
+	geoData, err := geo.GetMyLocation(*city)
 	if err != nil {
-		output.PrintError("Неверный формат URL/Неверный логин")
-		return
+		fmt.Println(err.Error())
 	}
-
-	vault = account.NewVault(files.NewJsonDb("data.vault"), *encrypter.NewEncrypter())
-	vault.AddAccount(*myAccount)
-}
-
-func dialogue() {
-	vault := account.NewVault(files.NewJsonDb("data.vault"), *encrypter.NewEncrypter())
-	err := godotenv.Load()
-	if err != nil {
-		output.PrintError("Не удалось найти env файл")
-	}
-Menu:
-	for {
-		choice := promptData("1. Создать аккаунт", "2. Найти аккаунт по url", "3. Найти аккаунт по логину", "4. Удалить аккаунт", "5. Выход", "Выберите вариант")
-		menuFunc := menu[choice]
-		if menuFunc == nil {
-			break Menu
-		}
-		menuFunc(vault)
-		// switch choice {
-		// case "1":
-		// 	createAccount(vault)
-		// case "2":
-		// 	findAccount(vault)
-		// case "3":
-		// 	deleteAccount(vault)
-		// default:
-		// 	break Menu
-		// }
-	}
-}
-
-func findAccountByUrl(vault *account.VaultWithDb) {
-	choice := promptData("Введите url, который хотите найти: ")
-	accs := vault.FindAccounts(choice, func(a account.Account, s string) bool { return strings.Contains(a.Url, s) })
-	if len(accs) == 0 {
-		fmt.Println("Нет аккаунтов с таким url")
-	} else {
-		for _, val := range accs {
-			fmt.Printf("Логин: %s Пароль: %s Url: %s\n", val.Login, val.Password, val.Url)
-		}
-	}
-}
-func findAccountByLogin(vault *account.VaultWithDb) {
-	choice := promptData("Введите url, который хотите найти: ")
-	accs := vault.FindAccounts(choice, func(a account.Account, s string) bool { return strings.Contains(a.Login, s) })
-	if len(accs) == 0 {
-		fmt.Println("Нет аккаунтов с таким login")
-	} else {
-		for _, val := range accs {
-			fmt.Printf("Логин: %s Пароль: %s Url: %s\n", val.Login, val.Password, val.Url)
-		}
-	}
-}
-func deleteAccount(vault *account.VaultWithDb) {
-	choice := promptData("Введите url, который хотите удалить: ")
-	vault.DeleteAccountsByURL(choice)
-	fmt.Println("Аккаунты удалены")
-}
-
-func promptData(prompt ...any) string {
-	for index, value := range prompt {
-		if index == len(prompt)-1 {
-			fmt.Printf("%v: ", value)
-		} else {
-			fmt.Println(value)
-		}
-	}
-	var res string
-	fmt.Scanln(&res)
-	return res
-}
-
-func factory() func(*account.VaultWithDb) {
-	return findAccountByUrl
+	fmt.Println(geoData.City)
+	weatherData := weather.GetWeather(*geoData, *format)
+	fmt.Println(weatherData)
 }
